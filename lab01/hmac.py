0a62f377f029314f77ff2e4d5d1d2fea7db8a946
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2022-10-03 22:05:25 -0400

    [lab02] abstracted hmac function

--- lab01/hmac.py
+++ lab01/hmac.py
@@ -20,28 +20,30 @@ class HmacEncoder:
     def encode(self, string: str) -> bytes:
         # encode the string to a byte array
         for block in this.parent.encode(string):
-            # left pad the key so it's the same size as block
-            b = len(block)
-            key_pad = [0]*(b - len(mac_key))
-            kplus = (key_pad + mac_key)
-            # repeat IPAD_TEMPLATE and OPAD_TEMPLATE (b/8) times
-            n_reps = (b//8)
-            ipad = IPAD_TEMPLATE * n_reps
-            opad = OPAD_TEMPLATE * n_reps
-            # XOR K+ and ipad
-            Si = xor(kplus, ipad)
-            # append Si and block
-            Si.extend(block)
-            # apply the hash to new Si (Si|M)
-            h0 = hash_init()
-            h0.update(debitize(Si))
-            hSiM = bitize(h0.digest())
-            # XOR K+ and opad
-            So = xor(kplus, opad)
-            # append So and h(Si|M)
-            So.extend(hSiM)
-            # hash (So|h(Si|M))
-            h1 = hash_init()
-            h1.update(debitize(So))
-            hmac = bitize(h1.digest())
-            yield hmac
+            yield self.hmac(block)
+
+    def hmac(self, block: 'list[int]') -> 'list[int]':
+        # left pad the key so it's the same size as block
+        b = len(block)
+        key_pad = [0]*(b - len(mac_key))
+        kplus = (key_pad + mac_key)
+        # repeat IPAD_TEMPLATE and OPAD_TEMPLATE (b/8) times
+        n_reps = (b//8)
+        ipad = IPAD_TEMPLATE * n_reps
+        opad = OPAD_TEMPLATE * n_reps
+        # XOR K+ and ipad
+        Si = xor(kplus, ipad)
+        # append Si and block
+        Si.extend(block)
+        # apply the hash to new Si (Si|M)
+        h0 = hash_init()
+        h0.update(debitize(Si))
+        hSiM = bitize(h0.digest())
+        # XOR K+ and opad
+        So = xor(kplus, opad)
+        # append So and h(Si|M)
+        So.extend(hSiM)
+        # hash (So|h(Si|M))
+        h1 = hash_init()
+        h1.update(debitize(So))
+        return bitize(h1.digest())
