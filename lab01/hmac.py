b91d817f6f8e854746818e5543e789eac0b79da0
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2022-10-03 22:59:46 -0400

    [lab02] documented HmacEncoder

--- lab01/hmac.py
+++ lab01/hmac.py
@@ -4,59 +4,75 @@ import logging
 class SimpleHmacEncoder:
     '''
     Encoder that performs HMAC encoding and HMAC check on decoding.
+    
+    This is a simple implementation of HMAC appended to the end of the
+    entire message.
     '''
 
-    def __init__(self, _parent: object, _mac_key: bytes)
+    def __init__(self, _parent: object, _mac_key: bytes):
         '''
         Initializes an encoder.
-        @param _encoding: str = the character set used to encode and
-            decode
+        @param _parent: object = backing encoder with encode and decode
+            methods
+        @param _mac_key: bytes = the mac key as bytes
         '''
         self.parent = _parent
         self.mac_key = _mac_key
 
     def encode(self, string: str) -> bytes:
         '''
-        Encodes the given string into bytes using self's character
-        encoding.
+        Encodes the given string using the backing encoder, then
+        appends its HMAC.
         @param string: str = to encode
-        @return byte encoding of the string
+        @return HMAC encoding of the string
         '''
         # delegate to parent encoder
         msg_byts = self._parent.encode(string)
-        # HMAC the result and return it
-        return hmac(msg_byts)
+        # HMAC the result
+        msg_mac = hmac(msg_byts)
+        # append to msg_byts
+        complete_msg = (msg_bytes + msg_mac)
+        # return the result
+        return complete_msg
 
     def decode(self, byts: bytes) -> str:
         '''
-        Decodes the given byte arrays into a string using self's
-        character encoding.
+        Separates the message from its HMAC, and decodes it using the
+        backing encoder.
         @param byts: bytes = to decode
-        @return string decoded from the byte array
+        @return message string decoded from the byte array
         '''
         # split byte array into message, theoretical MAC
-        msg_byts, theo_mac = byts[:-32], byts[-32:]
+        # sha256 is always 256 bytes = 32 bytes
+        msg_byts, theo_mac = (byts[:-32], byts[-32:])
         # calculate the mac from the message
         calc_mac = hmac(msg_byts)
         # compare MACs
         if (theo_mac != calc_mac):
-            # if different, give a warning
-            # but do NOT print the log MAC
-            logging.warning(f'Unexpected MAC: expected {theo_mac}')
-        else:
-            # otherwise, continue
-            logging.info(f'Message authenticated.')
+            # if different, throw an exception
+            # but do NOT include the calculated MAC
+            raise UnexpectedMac(f'Unexpected MAC: expected {theo_mac}')
+        # otherwise
         # delegate msg_byts to parent encoder
         return self._parent.decode(msg_byts)
     # end def decode(self, byts: bytes)
 
-    def hmac(self, byts: bytes) -> bytes:
+    def hmac(self, msg_byts: bytes) -> bytes:
+        '''
+        Finds the hashed MAC of the given message in bytes.
+        '''
         # append mac to msg_byts
         msg_mac = (msg_byts + self.mac_key)
         # hash msg_mac
         h0 = hash_init()
-        h0.update(debitize(msg_mac))
-        h_msg_mac = bitize(h0.digest())
+        h0.update(msg_mac)
         # return the result
+        return h0.digest()
     
 # end class SimpleHmacEncoder
+
+class UnexpectedMac(Exception):
+    '''
+    Thrown when a calculated MAC does not match the expected
+    theoretical MAC.
+    '''
