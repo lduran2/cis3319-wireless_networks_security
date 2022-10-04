617a1282dbb3b671a4aea41f38292d9591c25e46
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2022-10-03 22:21:28 -0400

    [lab02] simple hmac started

---
+++ lab01/hmac.py
@@ -0,0 +1,43 @@
+from hashlib import sha256
+
+class HmacEncoder:
+    '''
+    Encoder that performs HMAC encoding and HMAC check on decoding.
+    '''
+
+    def __init__(self, _parent: object, _mac_key: bytes)
+        '''
+        Initializes an encoder.
+        @param _encoding: str = the character set used to encode and
+            decode
+        '''
+        self.parent = _parent
+        self.mac_key = _mac_key
+
+    def encode(self, string: str) -> bytes:
+        '''
+        Encodes the given string into bytes using self's character
+        encoding.
+        @param string: str = to encode
+        @return byte encoding of the string
+        '''
+        msg_byts = self._parent.encode(self.encoding)
+        # append mac to msg_byts
+        msg_mac = msg_byts + self.mac_key
+        # hash msg_mac
+        h0 = hash_init()
+        h0.update(debitize(msg_mac))
+        h_msg_mac = bitize(h0.digest())
+        # return the result
+        return h_msg_mac
+
+    def decode(self, byts: bytes) -> str:
+        '''
+        Decodes the given byte arrays into a string using self's
+        character encoding.
+        @param byts: bytes = to decode
+        @return string decoded from the byte array
+        '''
+        
+        return byts.decode(self.encoding)
+# end class HmacEncoder
