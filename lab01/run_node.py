6cadadcc0c365e1a1964d9cdad467117db42022f
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2022-11-25 20:56:27 -0500

    [lab01] abstract run_node.run_node from run_node.main

--- lab01/run_node.py
+++ lab01/run_node.py
@@ -15,7 +15,7 @@ KEY_FILE = 'key.txt'
 # ends the input stream
 SENTINEL = 'exit'
 
-def receiveThread(node, des, encoding, prompt):
+def receiveThread(node, des, charset, prompt):
     old_tb = None
     while True:
         try:
@@ -27,7 +27,7 @@ def receiveThread(node, des, encoding, prompt):
             # ignore any illegal bytes
             msg_bytes = bytes(b for b in msg_bytes if b in range(256))
             # decrypt the message
-            dec_string = des.decrypt(msg_bytes, encoding=encoding)
+            dec_string = des.decrypt(msg_bytes, encoding=charset)
             # log the message received
             print(file=stderr)
             print(file=stderr)
@@ -47,16 +47,33 @@ def receiveThread(node, des, encoding, prompt):
             old_tb = tb
             continue
     # end while True
-# end def receiveThread(node, des, encoding)
+# end def receiveThread(node, des, charset)
 
 # run the node until SENTINEL is given
-def main(connecting_status: str, node_init: 'Callable[[addr, port], Node]', addr: str, port: int, encoding: str, prompt: str):
+def main(connecting_status: str, node_init: 'Callable[[addr, port], Node]', addr: str, port: int, charset: str, prompt: str):
+    '''
+    Run the node until SENTINEL is input using the given parameters.
+    This implementation handles the node's entire life cycle from
+    allocation to closing.
+    '''
     # configure the logger
     logging.basicConfig(level=logging.INFO)
-
     # create a node
     logging.info(f'{connecting_status} to {addr}:{port} . . .')
     node = node_init(addr, port)
+    # encode and send user input, decode messages received
+    run_node(node, charset, prompt)
+    # close the node
+    node.close()
+# end main(connecting_status: str, node_init: 'Callable[[addr, port], Node]', addr: str, port: int, charset: str, prompt: str)
+
+def run_node(node: Node, charset: str, prompt: str):
+    '''
+    Runs an existing node until SENTINEL is read from standard input.
+    '''
+    # configure the logger
+    logging.basicConfig(level=logging.INFO)
+
     # read in the key word
     key = KeyManager.read_key(KEY_FILE)
     # generate the DES key for encryption
@@ -64,7 +81,7 @@ def main(connecting_status: str, node_init: 'Callable[[addr, port], Node]', addr
     des = DES(key)
 
     # start the receiving thread
-    start_new_thread(receiveThread, (node, des, encoding, prompt))
+    start_new_thread(receiveThread, (node, des, charset, prompt))
 
     while True:
         # TODO: your code here
@@ -76,12 +93,10 @@ def main(connecting_status: str, node_init: 'Callable[[addr, port], Node]', addr
         
         # TODO: your code here
         # encryption
-        cyp_bytes = des.encrypt(msg_string, encoding=encoding)
+        cyp_bytes = des.encrypt(msg_string, encoding=charset)
         # send the message
         logging.info(f'Sending cypher: {cyp_bytes}')
         node.send(cyp_bytes)
     # end while True
 
-    # close the node
-    node.close()
-# end main(connecting_status: str, node_init: 'Callable[[addr, port], Node]', addr: str, port: int, encoding: str, prompt: str)
+# end run_node(node: Node, charset: str, prompt: str)
