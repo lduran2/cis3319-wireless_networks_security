bf79e623716ff8a0f9289a8370bad6cf8f6a769f
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2022-11-30 02:59:29 -0500

    [lab05-v] abstracted receive_service_request

--- lab01/V_server.py
+++ lab01/V_server.py
@@ -33,12 +33,45 @@ def main(node_data, server_data):
     # read the key and create DES for TGS/V
     DES_v = DES(KeyManager.read_key(config['kerberos_keys']['Kv_file']))
 
+    # (c) client/server authentication exchange to obtain service
+    # check for service-granting ticket request with valid ticket
+    service_request = receive_service_request(server, server_data.charset, DES_v)
+    if (not(service_request)):
+        return
+    # split the service request
+    Authenticator_c, DES_c_v = service_request
+
+    # decrypt Authenticator_c2
+    # 1st encode Authenticator_c2 to the key charset
+    # this includes 0 bytes
+    cipher_Authenticator_c2_byts_untrim = Authenticator_c.encode(KEY_CHARSET)
+    # trim last 0 bytes
+    cipher_Authenticator_c2_byts = bytes.rstrip(cipher_Authenticator_c2_byts_untrim, b'\x00')
+    logging.info(f'(5) Received bytes: {cipher_Authenticator_c2_byts}')
+    # decrypt Authenticator_c2
+    plain_Authenticator_c2 = DES_c_v.decrypt(cipher_Authenticator_c2_byts)
+    print()
+    # split Authenticator_c2
+    ID_c2, AD_c2, TS5_str = plain_Authenticator_c2.split('||')
+    # parse the timestamp TS5
+    TS5 = float(TS5_str.rstrip('\0'))
+
+    send_service(server, DES_c_v, TS5)
+
+    # encode and send user input, decode messages received
+    run_node.run_node(server, server_data.charset, node_data.prompt)
+    # close the node
+    server.close()
+# end def main(node_data, server_data)
+
+
+def receive_service_request(server, charset, DES_v):
     # (5Rx) C -> V: Ticket_v || Authenticator_c
 
     # receive the message
     msg_bytes = run_node.recv_blocking(server)
     # decode the message
-    msg_chars = msg_bytes.decode(server_data.charset)
+    msg_chars = msg_bytes.decode(charset)
     # log the message received
     logging.info(f'(5Rx) Received: {msg_bytes}')
     # print the decoded message
@@ -46,7 +79,7 @@ def main(node_data, server_data):
     print('(5Rx) Decoded: ', end='', file=stderr, flush=True)
     print(msg_chars)
     # split the message
-    cipher_Ticket_v_chars, cipher_Authenticator_c2_chars = msg_chars.split('||')
+    cipher_Ticket_v_chars, Authenticator_c = msg_chars.split('||')
 
     # decrypt the Ticket_v
     # 1st encode the ticket to the key charset
@@ -76,41 +109,17 @@ def main(node_data, server_data):
         # send expiration message
         server.send(cipher_expire)
         # listen for a new message
-        return
+        return False
     # end if (now - TS2_1o >= Lifetime2_1o)
 
-    # decrypt Authenticator_c2
-    # 1st encode Authenticator_c2 to the key charset
-    # this includes 0 bytes
-    cipher_Authenticator_c2_byts_untrim = cipher_Authenticator_c2_chars.encode(KEY_CHARSET)
-    # trim last 0 bytes
-    cipher_Authenticator_c2_byts = bytes.rstrip(cipher_Authenticator_c2_byts_untrim, b'\x00')
-    logging.info(f'(5) Received bytes: {cipher_Authenticator_c2_byts}')
-    # decrypt Authenticator_c2
-    plain_Authenticator_c2 = DES_c_v.decrypt(cipher_Authenticator_c2_byts)
-    print()
-    # split Authenticator_c2
-    ID_c2, AD_c2, TS5_str = plain_Authenticator_c2.split('||')
-    # parse the timestamp TS5
-    TS5 = float(TS5_str.rstrip('\0'))
-
-    # send a message for successful authentication
-    plain_success = f'{TS5 + 1}'
-    cipher_success = DES_c_v.encrypt(plain_success)
-    server.send(cipher_success)
-
-    print(file=stderr)
-
-    # encode and send user input, decode messages received
-    run_node.run_node(server, server_data.charset, node_data.prompt)
-    # close the node
-    server.close()
-# end def main(node_data, server_data)
+    return (Authenticator_c, DES_c_v)
+# end def receive_service_request(server, charset, DES_v)
 
 
 def send_service(server, DES_c_v, TS5):
     # send a message for successful authentication
-    cipher_success = DES_c_v.encrypt(str(TS5 + 1))
+    plain_success = f'{TS5 + 1}'
+    cipher_success = DES_c_v.encrypt(plain_success)
     server.send(cipher_success)
 
     print(file=stderr)
